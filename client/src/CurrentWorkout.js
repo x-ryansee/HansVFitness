4baa69f89ec33e1073025165002c59a2d37e1b59
Ryan See <ryanjosephsee@gmail.com>
2023-04-06 14:54:28 -0700

    updated currentworkout and added seed data

--- client/src/CurrentWorkout.js
+++ client/src/CurrentWorkout.js
@@ -1,86 +1,122 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
-function CurrentWorkout() {
-  const [workouts, setWorkouts] = useState([]);
-  const [selectedWorkout, setSelectedWorkout] = useState(null);
-  const [weight, setWeight] = useState(null);
-  const [reps, setReps] = useState(null);
-  const [sets, setSets] = useState(null);
+const CurrentWorkout = () => {
+  const [exercises, setExercises] = useState([{ name: '', sets: [{ weight: '', reps: '' }] }]);
 
-  useEffect(() => {
-    axios.get('/api/workouts')
-      .then(response => {
-        setWorkouts(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }, []);
+  const handleExerciseChange = (index, event) => {
+    const values = [...exercises];
+    values[index].name = event.target.value;
+    setExercises(values);
+  };
 
-  function handleWorkoutSelect(event) {
-    setSelectedWorkout(event.target.value);
-  }
+  const handleWeightChange = (exIndex, setIndex, event) => {
+    const values = [...exercises];
+    values[exIndex].sets[setIndex].weight = event.target.value;
+    setExercises(values);
+  };
 
-  function handleWeightChange(event) {
-    setWeight(event.target.value);
-  }
+  const handleRepsChange = (exIndex, setIndex, event) => {
+    const values = [...exercises];
+    values[exIndex].sets[setIndex].reps = event.target.value;
+    setExercises(values);
+  };
 
-  function handleRepsChange(event) {
-    setReps(event.target.value);
-  }
+  const handleAddExercise = () => {
+    const values = [...exercises];
+    values.push({ name: '', sets: [{ weight: '', reps: '' }] });
+    setExercises(values);
+  };
 
-  function handleSetsChange(event) {
-    setSets(event.target.value);
-  }
+  const handleAddSet = (index) => {
+    const values = [...exercises];
+    values[index].sets.push({ weight: '', reps: '' });
+    setExercises(values);
+  };
 
-  function handleSubmit(event) {
+  const handleRemoveExercise = (index) => {
+    const values = [...exercises];
+    values.splice(index, 1);
+    setExercises(values);
+  };
+
+  const handleRemoveSet = (exIndex, setIndex) => {
+    const values = [...exercises];
+    values[exIndex].sets.splice(setIndex, 1);
+    setExercises(values);
+  };
+
+  const handleSubmit = (event) => {
     event.preventDefault();
-    axios.post('/api/exercise_sets', {
-      exercise_set: {
-        exercise_id: selectedWorkout,
-        weight: weight,
-        reps: reps,
-        sets: sets
-      }
-    })
-      .then(response => {
+    axios.post('/exercise_sets', { exercise_sets: exercises })
+      .then((response) => {
         console.log(response.data);
+        alert('Workout saved successfully!');
       })
-      .catch(error => {
-        console.error(error);
+      .catch((error) => {
+        console.log(error);
+        alert('There was an error saving the workout.');
       });
-  }
+  };
 
   return (
     <div>
-      <h2>Track a Workout</h2>
       <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="workout-select">Select a Workout:</label>
-          <select id="workout-select" onChange={handleWorkoutSelect}>
-            <option value="">Select a workout</option>
-            {workouts.map(workout => (
-              <option key={workout.id} value={workout.id}>{workout.name}</option>
+        {exercises.map((exercise, exIndex) => (
+          <div key={exIndex}>
+            <label>
+              Exercise:
+              <select value={exercise.name} onChange={(event) => handleExerciseChange(exIndex, event)}>
+                <option value="">Select an exercise</option>
+                {/* Replace with list of exercises from database */}
+                <option value="Bench Press">Bench Press</option>
+                <option value="Squats">Squats</option>
+                <option value="Deadlifts">Deadlifts</option>
+              </select>
+            </label>
+            {exercise.sets.map((set, setIndex) => (
+              <div key={setIndex}>
+                <label>
+                  Weight:
+                  <input
+                    type="text"
+                    value={set.weight}
+                    onChange={(event) => handleWeightChange(exIndex, setIndex, event)}
+                  />
+                </label>
+                <label>
+                  Reps:
+                  <input
+                    type="text"
+                    value={set.reps}
+                    onChange={(event) => handleRepsChange(exIndex, setIndex, event)}
+                  />
+                </label>
+                {setIndex > 0 && (
+                  <button type="button" onClick={() => handleRemoveSet(exIndex, setIndex)}>
+                    -
+                  </button>
+                )}
+                {setIndex === exercise.sets.length - 1 && (
+                  <button type="button" onClick={() => handleAddSet(exIndex)}>
+                    +
+                  </button>
+                )}
+              </div>
             ))}
-          </select>
-        </div>
-        <div>
-          <label htmlFor="weight-input">Weight (in pounds):</label>
-          <input id="weight-input" type="number" min="0" onChange={handleWeightChange} />
-        </div>
-        <div>
-          <label htmlFor="reps-input">Reps:</label>
-          <input id="reps-input" type="number" min="0" onChange={handleRepsChange} />
-        </div>
-        <div>
-          <label htmlFor="sets-input">Sets:</label>
-          <input id="sets-input" type="number" min="0" onChange={handleSetsChange} />
-        </div>
-        <button type="submit" disabled={!selectedWorkout || !weight || !reps || !sets}>Submit</button>
+            <button type="button" onClick={() => handleRemoveExercise(exIndex)}>
+              Remove Exercise
+            </button>
+          </div>
+        ))}
+        <button type="button" onClick={handleAddExercise}>
+          Add Another Exercise
+        </button>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
-}
+
+                }
 
 export default CurrentWorkout
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,61 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Users
+
+User.create(name: "John Smith", email: "john@example.com", password: "password", role: "user")
+User.create(name: "Jane Doe", email: "jane@example.com", password: "password", role: "user")
+User.create(name: "Admin User", email: "admin@example.com", password: "password", role: "admin")
+
+
+
+# Exercises
+
+Exercise.create([
+    { name: "Bench Press", muscle_group: "Chest", description: "Lie on a bench and lower a barbell to your chest, then press it back up."},
+    { name: "Squat", muscle_group: "Legs", description: "Stand with a barbell on your shoulders and squat down until your thighs are parallel to the ground."},
+    { name: "Deadlift", muscle_group: "Back", description: "Lift a barbell off the ground with straight arms and lift it up until you're standing straight."},
+    { name: "Overhead Press", muscle_group: "Shoulders", description: "Stand with a barbell on your shoulders and press it up overhead."},
+    { name: "Pull-up", muscle_group: "Back", description: "Hang from a bar and pull yourself up until your chin is above the bar."},
+    { name: "Dumbbell Fly", muscle_group: "Chest", description: "Lie on a bench with dumbbells and open your arms wide, then bring them back up."},
+    { name: "Bicep Curl", muscle_group: "Arms", description: "Stand with dumbbells and curl them up towards your shoulders."},
+    { name: "Tricep Extension", muscle_group: "Arms", description: "Hold a dumbbell behind your head with both hands, then straighten your arms."},
+    { name: "Leg Press", muscle_group: "Legs", description: "Sit on a machine and press your legs against a platform."},
+    { name: "Leg Curl", muscle_group: "Legs", description: "Lie on a machine and curl your legs towards your buttocks."},
+    { name: "Lat Pulldown", muscle_group: "Back", description: "Sit on a machine and pull a bar down towards your chest."},
+    { name: "Hammer Curl", muscle_group: "Arms", description: "Stand with dumbbells and curl them up with your palms facing each other."},
+    { name: "Calf Raise", muscle_group: "Legs", description: "Stand on a machine and raise your heels up and down."},
+    { name: "Seated Row", muscle_group: "Back", description: "Sit on a machine and pull a handle towards your torso."},
+    { name: "Push-up", muscle_group: "Chest", description: "Start in a plank position and lower your body down and then press it back up."},
+    { name: "Lateral Raise", muscle_group: "Shoulders", description: "Stand with dumbbells at your sides and lift them up to shoulder height."},
+    { name: "Skullcrusher", muscle_group: "Arms", description: "Lie on a bench with a barbell and lower it towards your forehead, then lift it back up."},
+    { name: "Plank", muscle_group: "Core", description: "Start in a push-up position and hold your body straight for as long as you can."},
+    { name: "Russian Twist", muscle_group: "Core", description: "Sit on the ground with your feet in the air and twist your torso from side to side while holding a medicine ball."},
+    { name: "Flutter Kick", muscle_group: "Core", description: "Lie on your back and flutter your legs up and down while keeping them straight."}
+  ])
+  
+
+
+  # Workouts
+john = User.find_by(name: "John Smith")
+jane = User.find_by(name: "Jane Doe")
+
+Workout.create(date: Date.today, user: john)
+Workout.create(date: Date.yesterday, user: jane)
+
+# Exercise Sets
+bench_press = Exercise.find_by(name: "Bench Press")
+squats = Exercise.find_by(name: "Squats")
+deadlifts = Exercise.find_by(name: "Deadlifts")
+
+john_today_workout = Workout.find_by(date: Date.today, user: john)
+jane_yesterday_workout = Workout.find_by(date: Date.yesterday, user: jane)
+
+ExerciseSet.create(exercise: bench_press, workout: john_today_workout, weight: 100, reps: 8)
+ExerciseSet.create(exercise: squats, workout: john_today_workout, weight: 150, reps: 10)
+ExerciseSet.create(exercise: deadlifts, workout: john_today_workout, weight: 200, reps: 6)
+
+ExerciseSet.create(exercise: bench_press, workout: jane_yesterday_workout, weight: 80, reps: 12)
+ExerciseSet.create(exercise: squats, workout: jane_yesterday_workout, weight: 120, reps: 8)
+ExerciseSet.create(exercise: deadlifts, workout: jane_yesterday_workout, weight: 180, reps: 6)
